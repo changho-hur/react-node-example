8b93e275c39bfe8101dfb9938d2926f73beece87
changhohur <changho.hur@blucean.net>
2020-04-10 11:30:59 +0900

    로컬에 빌드 하고 실행하기.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.all('/*', function (req, res, next){
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE');
     //res.header('Access-Control-Allow-Headers','X-Requested-With');
     next();
